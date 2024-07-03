072bf71e65883880081063a5b19b15c74f52ec10
Sahil Mukesh Ambre <59484827+sahilambre@users.noreply.github.com>
2024-07-03 17:12:52 -0400

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553;
+let accountEmail = "sahil@google.com";
+var accountPassword = "12345";
+accountCity = "Mumbai";
+let accountState;
+
+accountEmail = "sa@gmail.com";
+
+console.log(accountId);
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello");
+
+let name = "sahil"; // string
+let age = 23; // number
+let isLoggedIn = false; // bool
+let state = null; //null
+
+// number = 2 to power 53 range
+// bigint
+// string => ''/""
+//boolena => true/false
+//* null => standalone value
+//* undefined => value not assigned yet
+//* symbol => unique
+
+//* object
+
+console.log(typeof name);
+console.table([
+  typeof name,
+  typeof age,
+  typeof isLoggedIn,
+  typeof state,
+  typeof undefined,
+  typeof null,
+]);
